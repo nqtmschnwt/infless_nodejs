5ed9b8acff598a5cda4513a3163850cc1abb5ac0
mschnwt <tung.ng1991@gmail.com>
2022-11-14 18:41:29 +0700

    fix bug

--- route/web.js
+++ route/web.js
@@ -48,9 +48,9 @@ let initWebRoutes = (app) => {
       router.post("/securities/apphome", homeController.postAppHomePage);
       // Login page
       router.get("/securities/login", checkAuthenticated, loginController.getLoginPage);
+      router.get("/login", checkAuthenticated, loginController.getLoginPage);
       //router.post("/login", limiter(1,5,'Đăng nhập không thành công quá nhiều lần. Bạn sẽ không thể đăng nhập trong vòng 1 giờ nữa.'), loginController.postLoginPage);
       router.post("/securities/login", loginController.postLoginPage);
-      router.post("/login", loginController.postLoginPage);
       // Login from external systems
       router.post("/securities/app/login", apiController.externalLogin);
       // Logout
